d9c26fe8a99eab6a5f04a6d965b094c584502fc5
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2024-07-02 09:01:02 +0200

    create multisite setup script

---
+++ scripts/setup-multisite.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# This script is used to setup a multisite environment for Grav CMS.
+# Usage: ./scripts/setup-multisite.sh <environment> <clone URL>
+
+cd "$(dirname "$0")/.."
+grav_root=$(pwd)
+environment=$1
+cloneURL=$2
+
+# Check if the environment and clone URL is provided
+if [ -z "$environment" ]; then
+    echo "Please provide the environment name."
+    exit 1
+fi
+if [ -z "$cloneURL" ]; then
+    echo "Please provide the clone URL."
+    exit 1
+fi
+
+# Check if the environment already exists
+if [ -d "$grav_root/user/env/$environment" ]; then
+    echo "The environment already exists."
+    exit 1
+fi
+
+# Clone Git repository
+git clone $cloneURL user/env/$environment
+
+# TODO: get paths from config/streams.yaml
+# Create necessary directories
+mkdir backup/$environment
+mkdir cache/$environment
+mkdir images/$environment
+mkdir logs/$environment
